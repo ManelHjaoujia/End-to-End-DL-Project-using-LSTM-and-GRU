e532163f3492e707f83b81288dd0bfe0f1998f80
manel <manelemails@gmail.com>
2025-11-04 13:13:23 +0100

    fix the model path

--- LSTM/app.py
+++ LSTM/app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import numpy as np
+import os
 import pickle
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.sequence import pad_sequences
